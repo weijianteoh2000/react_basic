f4ab8051b3c1396b7aae86f96b89313d3e337ae3
Teoh Wei Jian <weijianteoh2000@graduate.utm.my>
2024-06-26 17:52:41 +0800

    Change bat file statement

--- jenkins/scripts/deliver.bat
+++ jenkins/scripts/deliver.bat
@@ -1,10 +1,14 @@
-#!/usr/bin/env sh
+@echo off
 
 npm run build
 
-npm start &
-sleep 1
-echo $! > .pidfile
+start npm start
+timeout /t 1 /nobreak
+for /f "tokens=2 delims==; " %%I in ('tasklist /fi "imagename eq node.exe" /fo csv /nh') do (
+  echo %%I > .pidfile
+  goto break
+)
+:break
 
-echo 'Now...'
-echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
+echo Now...
+echo Visit http://localhost:3000 to see your Node.js/React application in action.
--- jenkins/scripts/kill.bat
+++ jenkins/scripts/kill.bat
@@ -1,3 +1,10 @@
-#!/usr/bin/env sh
+@echo off
 
-kill $(cat .pidfile)
+REM Read the PID from the .pidfile
+set /p PID=<.pidfile
+
+REM Kill the process using the PID
+taskkill /PID %PID% /F
+
+REM Optionally, delete the .pidfile after killing the process
+del .pidfile
